9876c90dddca59e834760a54ac90f7e758a45da6
bharath <bharath@neokred.tech>
2026-05-07 10:24:50 +0530

    fix: update benchmark script for JNI execution

--- rlog-benchmark/src/jmh/java/org/apache/logging/benchmark/LoggingBenchmark.java
+++ rlog-benchmark/src/jmh/java/org/apache/logging/benchmark/LoggingBenchmark.java
@@ -16,38 +16,8 @@ import java.io.File;
 @Measurement(iterations = 2, time = 5, timeUnit = TimeUnit.SECONDS)
 public class LoggingBenchmark {
 
-    private Logger standardLogger;
-    private LoggerContext ctx;
-
-    @Setup(Level.Trial)
-    public void setup() {
-        // Force Log4j2 to use Async Loggers globally
-        System.setProperty("log4j2.contextSelector", "org.apache.logging.log4j.core.async.AsyncLoggerContextSelector");
-        
-        // Initialize standard Log4j 2 core explicitly to bypass RlogProvider if needed
-        File configFile = new File("src/jmh/resources/log4j2-async.xml");
-        ctx = Configurator.initialize("BenchmarkContext", null, configFile.toURI());
-        standardLogger = ctx.getLogger(LoggingBenchmark.class.getName());
-        
-        // Initialize our Native FFM logger
-        // NativeLogger is initialized automatically in its static block
-        NativeLogger.log(3, "Benchmarking starting...");
-    }
-
-    @TearDown(Level.Trial)
-    public void tearDown() {
-        if (ctx != null) {
-            Configurator.shutdown(ctx);
-        }
-    }
-
     @Benchmark
-    public void benchmarkStandardLog4j2() {
-        standardLogger.info("This is a standard Log4j2 Async log message");
-    }
-
-    @Benchmark
-    public void benchmarkRlog4jFFM() {
-        NativeLogger.log(3, "This is an Rlog4 FFM log message");
+    public void benchmarkRlog4jJNI() {
+        NativeLogger.log(3, "This is an Rlog4 JNI log message");
     }
 }
